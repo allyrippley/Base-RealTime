8e1cef0c4e20d2f7c242ea5974ac46f40c761782
A Rippley <mctransporter@gmail.com>
2015-03-25 03:00:00 +0600

    Commit

--- 1.js
+++ 1.js
@@ -1,3 +1,14 @@
+function printHelp() {
+  console.log("1.js (c) Ally Rippley");
+  console.log("");
+  console.log("usage");
+  console.log("--help     print this help");
+  console.log("--name     say hello to {NAME}");
+  console.log("");
+  console.log("");
+
+}
+
 var args = require("minimist")(process.argv.slice(2),{
   string: "name"
 });
