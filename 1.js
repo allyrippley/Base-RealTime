084671300802d1209b53aaf64844e2699255810a
A Rippley <mctransporter@gmail.com>
2015-03-29 03:00:00 +0600

    Commit

--- 1.js
+++ 1.js
@@ -2,8 +2,8 @@ function printHelp() {
   console.log("1.js (c) Ally Rippley");
   console.log("");
   console.log("usage");
-  console.log("--help     print this help");
-  console.log("--name     say hello to {NAME}");
+  console.log("--help            print this help");
+  console.log("--file={NAME}     read the file of {NAME}");
   console.log("");
   console.log("");
 
@@ -13,6 +13,14 @@ var args = require("minimist")(process.argv.slice(2),{
   string: "name"
 });
 
-var name = args.name;
 
-console.log("Hello " + name);
+if (args.help || !args.file) {
+  printHelp();
+  process.exit(1);
+}
+
+var hello = require("./testing.js");
+
+
+var contents = hello.say(args.file);
+console.log(contents.toString());
--- testing.js
+++ testing.js
@@ -1,6 +1,6 @@
 function say(filename) {
   var contents = fs.readFileSync(filename);
-
+  return contents;
 }
 
 var fs = require("fs");
